2dbf6a4e9feec317b77aff7e7021a5d89489c660
Manuel Garnica <manuel.garnica@elconde.org>
2019-04-17 09:35:35 -0500

    fdsf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
+import { TemplateModule } from './template-adminlte/template-adminlte.module';
+import { TemplateIntranetModule } from './template-intranet/template-intranet.module';
+import { TemplateContenidoModule } from './template-contenido/template-contenido.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgProgressModule, NgProgressInterceptor } from 'ngx-progressbar';
+import { NgxUiLoaderModule, NgxUiLoaderHttpModule } from  'ngx-ui-loader'; 
+import { NgxUiLoaderConfig, SPINNER_TYPES,  NGX_POSITIONS, PB_DIRECTIONS } from 'ngx-ui-loader';
 
-import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+ 
+                       
+
 import { AppComponent } from './app.component';
-
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  bgsColor: '#E6E6E6',
+  fgsColor: '#E6E6E6',
+  bgsPosition: NGX_POSITIONS.centerCenter,
+  fgsSize: 150,
+  overlayColor: 'rgba(180, 175, 175, 0.8)',
+  bgsOpacity: 5,
+  fgsType: SPINNER_TYPES.circle,
+  pbDirection: PB_DIRECTIONS.leftToRight, // progress bar direction
+  pbThickness: 5, // progress bar thickness
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+     HttpClientModule
+    ,NgProgressModule
+    ,BrowserModule
+    ,AppRoutingModule
+    ,NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
+    ,NgxUiLoaderHttpModule
+    ,TemplateModule
+    ,TemplateIntranetModule
+    ,TemplateContenidoModule
+    ,BrowserAnimationsModule
+   
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: NgProgressInterceptor, multi: true }
+     ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+	}
